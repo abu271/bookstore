7296b23cc665720df2d3a7829ea5229c44dacd00
Abu Darda <38472867+abu271@users.noreply.github.com>
2024-12-23 12:51:14 +0000

    Feat/list books (#2)
    List books from book API
    
    * add proxy in vite config to bypass CORS
    * create Book component to display details
    * install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './public/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+import { Book } from './Book';
+
+async function getBooks() {
+  try {
+    const {data} = await axios.get('/api/book/books');
+    const list = data.map(book => <Book key={book.id} {...book} />);
+    return list;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    getBooks().then(setBooks);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {books ? books : 'Loading...'}
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Book.jsx
@@ -0,0 +1,10 @@
+export const Book = (book) => {
+  return (
+    <div>
+      <h1>Title: {book.name}</h1>
+      <h2>Author: {book.author}</h2>
+      <p>Edition: {book.edition}</p>
+      <p>Publication year: {book.publication_year}</p>
+    </div>
+  );
+}
