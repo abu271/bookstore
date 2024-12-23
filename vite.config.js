c6b179438c248ff398f26c457e810edf08641998
Abu <abudarda166@gmail.com>
2024-12-23 17:00:42 +0000

    Remove proxy because API allows 3004 origin

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3004,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
+    port: 3004
   },
   plugins: [react()],
 })
