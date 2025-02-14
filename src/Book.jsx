163b3d97a693f653f85955a7d260541da7a2e1b8
Abu Darda <38472867+abu271@users.noreply.github.com>
2025-02-14 16:41:07 +0000

    fix: correct author typo (#9)

--- src/Book.jsx
+++ src/Book.jsx
@@ -2,7 +2,7 @@ export const Book = (book) => {
   return (
     <div>
       <h1>Title: {book.name}</h1>
-      <h2>Author: {book.author}</h2>
+      <h2>Author: {book.authors}</h2>
       <p>Edition: {book.edition}</p>
       <p>Publication year: {book.publication_year}</p>
     </div>
