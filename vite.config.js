9983b95ecca1261b8de7ceb6a1e3c6a53309ee1f
Abu <abudarda166@gmail.com>
2025-03-23 02:23:54 +0000

    loadEnv in config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3004
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      port: 3004
+    },
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  }
 })
